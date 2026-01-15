23fb15c0a686b51da1b58753f75270c4b1bea737
ujaRHR <rhraju.dev@gmail.com>
2026-01-15 20:44:01 +0600

    fix: ui color schemes

--- internal/ui/colors.go
+++ internal/ui/colors.go
@@ -1,17 +1,15 @@
 package ui
 
-func UIColors() {
-	const (
-		Reset = "\033[0m"
+const (
+	Reset = "\033[0m"
 
-		Red    = "\033[31m"
-		Green  = "\033[32m"
-		Yellow = "\033[33m"
-		Blue   = "\033[34m"
-		Purple = "\033[35m"
-		Cyan   = "\033[36m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Purple = "\033[35m"
+	Cyan   = "\033[36m"
 
-		Bold = "\033[1m"
-		Dim  = "\033[2m"
-	)
-}
+	Bold = "\033[1m"
+	Dim  = "\033[2m"
+)
